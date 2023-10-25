b8cc81f094abb0064509ee16c9a0135c48e508e4
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-25 16:13:54 +0200

    feat: add type model and values to proj. seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,7 +21,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $types = Type::all()->pluck("id");
+        $types[] = null;
+
         for ($i = 0; $i < 10; $i++) {
+            $type_id = $faker->randomElement($types);
+
             $project = new Project();
 
             $project->author = $faker->firstNameMale();
@@ -28,6 +34,9 @@ class ProjectSeeder extends Seeder
             $project->slug = Str::slug($project->title);
             $project->link = $faker->catchPhrase();
             $project->date = $faker->dateTime();
+
+            $project->type_id = $type_id;
+
             $project->description = $faker->paragraphs(1, true);
 
             $project->save();
